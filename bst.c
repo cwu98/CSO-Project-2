f4a4ce4e21f81d37409554aca72acbe554755926
Cary Wu <cw2535@nyu.edu>
2019-03-07 13:02:59 -0500

    initial commit

--- bst.c
+++ bst.c
@@ -5,12 +5,30 @@
 
 
 void add ( bst_node ** root, char * word ) {
-    
+  bst_node newNode;
+  bst_node *currNode = *root;
+  while (currNode != NULL) {
+    int compare = strcmp(*word, currNode->data);
+    if (compare < 0) { //word is smaller than current node's data, go left
+      currNode = &currNode->left;
+    }
+    else { //go right
+      currNode = &currNode->right;
+    }
+  }
+  *currNode = newNode;
+  newNode.data = *word;
 }
 
  
 void inorder ( bst_node * root ) {
-    
+  if(root == NULL){
+    printf("\n");
+    return;
+  }
+  inorder(&root->left);
+  printf("&s \n", *root->data);
+  inorder(&root->right);
 }
 
 
