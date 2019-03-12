d50f4bb9932fc1723368652dd1fb44aa1bb7b144
Cary Wu <cw2535@access2.cims.nyu.edu>
2019-03-12 19:58:16 -0400

    added comments

--- float.c
+++ float.c
@@ -26,8 +26,8 @@ float get_M  ( float f ) {
   int Eval = get_E(f);
   for(i=22; i>=0; i--){
     foo = temp&1 << i;
-    if (foo != 0){
-      frac += powf(2, -1*(23-i));
+    if (foo != 0){//1 at the bit position
+      frac += powf(2, -1*(23-i)); //calculate frac
     }
   }
   if(Eval == -126){//denormalized, exp all 0s
@@ -42,14 +42,14 @@ float get_M  ( float f ) {
     }
   }
   else{
-    mantissa = 1.0 + frac;
+    mantissa = 1.0 + frac; //add 1.0 to frac to get mantissa
   }
   return mantissa;
 }
 
 
 int get_s ( float f ) {
-  if (signbit(f)){
+  if (signbit(f)){ //signbit is used because it returns -1 for -nan
     return -1;
   }
 	return 1;
@@ -65,16 +65,16 @@ int get_E ( float f ) {
   //exp uses bits 23 - 30 (8 bits)
   for (i=30; i>=23; i--){
     bar = tmp&1 << i;
-    if(bar != 0){
-      exp += (int) (powf(2, i-23));
+    if(bar != 0){ //1 at the bit location
+      exp += (int) (powf(2, i-23)); //calculate exp
     }
   }
   if(exp == 0){ //denormalized, all zero
   
-    E = 1 - 127;
+    E = 1 - 127; //E = 1-bias 
   }
   else if(exp == 255 || exp == -255){ //special values
-    E = 255;
+    E = 255; 
   }
   else{ //by definition of E = exp - bias (which is 127 for single precision)
     E = exp - 127;
